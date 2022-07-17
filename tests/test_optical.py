0a352a6aeea91b5f5c8f64de99ce15988bf62455
Daniel McQuillen <daniel@mcquilleninteractive.com>
2022-07-18 09:56:09 +1000

    Update tests

--- tests/test_optical.py
+++ tests/test_optical.py
@@ -1,19 +1,28 @@
+import json
+from unittest import TestCase
+
 from py_igsdb_base_data.optical import OpticalStandardMethodResultsFactory, OpticalStandardMethodResults, \
     OpticalColorResultFactory, IntegratedSpectralAveragesSummaryValuesFactory, \
     IntegratedSpectralAveragesSummaryValues, OpticalColorResult
 
 
-def test_integrated_spectral_averages_summary_factory():
-    instance = IntegratedSpectralAveragesSummaryValuesFactory.create()
-    assert isinstance(instance, IntegratedSpectralAveragesSummaryValues)
-    assert instance.color.transmittance_front.direct_direct.trichromatic.x is None
+class TestPhysicalPropertiesDataclass(TestCase):
 
+    def test_integrated_spectral_averages_summary_factory(self):
+        instance = IntegratedSpectralAveragesSummaryValuesFactory.create()
+        self.assertIsInstance(instance, IntegratedSpectralAveragesSummaryValues)
 
-def test_optical_standard_method_results_factory():
-    instance = OpticalStandardMethodResultsFactory.create()
-    assert isinstance(instance, OpticalStandardMethodResults)
+    def test_optical_standard_method_results_factory(self):
+        instance = OpticalStandardMethodResultsFactory.create()
+        self.assertIsInstance(instance, OpticalStandardMethodResults)
 
+    def test_optical_color_result_factory(self):
+        instance = OpticalColorResultFactory.create()
+        self.assertIsInstance(instance, OpticalColorResult)
 
-def test_optical_color_result_factory():
-    instance = OpticalColorResultFactory.create()
-    assert isinstance(instance, OpticalColorResult)
+    def test_create_from_json(self):
+        with open('tests/data/valid_summary_values.json', 'r') as f:
+            summary_dict = json.load(f)
+
+        summary_values = IntegratedSpectralAveragesSummaryValues.from_dict(summary_dict)
+        self.assertEquals(0.5165770985449425, summary_values.solar.reflectance_back.direct_direct)
