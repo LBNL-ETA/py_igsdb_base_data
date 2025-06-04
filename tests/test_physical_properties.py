fc0ba46ab48175fc7a4bf8046b5a4ea620b97d9f
Daniel McQuillen <daniel@mcquilleninteractive.com>
2025-06-05 08:27:35 +1000

    Update test

--- tests/test_physical_properties.py
+++ tests/test_physical_properties.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
 
 from py_igsdb_base_data.product import PhysicalProperties
-
+from decimal import Decimal
 from src.py_igsdb_base_data.product import BlindGeometry
 
 
 class TestPhysicalPropertiesDataclass(TestCase):
-
     def test_physical_properties_dataclass_library(self):
         p = PhysicalProperties()
         # should automatically create optical properties
@@ -15,7 +14,7 @@ class TestPhysicalPropertiesDataclass(TestCase):
 
     def test_venetian_blind_get_rise_when_curvature_defined(self):
         p = BlindGeometry()
-        p.slat_width = "16"
-        p.slat_curvature = "1"
-        rise = p.rise # calls getter
-        self.assertEqual(rise, "8.0") 
+        p.slat_width = Decimal(16)
+        p.slat_curvature = Decimal(1)
+        rise = p.rise  # calls getter
+        self.assertEqual(rise, Decimal(8.0))
