9e36aba94be6ca368cb130947caf62afb3180e53
Daniel McQuillen <daniel@mcquilleninteractive.com>
2026-05-26 12:47:24 +1000

    Update comment

--- src/py_igsdb_base_data/product.py
+++ src/py_igsdb_base_data/product.py
@@ -842,7 +842,7 @@ class BaseProduct(IGSDBObject):
     @property
     def has_thermal_ir_wavelengths(self) -> bool:
         """
-        Returns True if the product has thermal IR wavelength data > 5 microns
+        Returns True if the product has thermal IR wavelength data > 25 microns
 
         Returns:
             bool
